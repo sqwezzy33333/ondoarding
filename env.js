16b12624b2c035bfb4fc62acce7fb06967363bc0
Sqwezzy33333 <sergey.kornev3@gmail.com>
2024-03-09 16:11:39 +0300

    init

---
+++ env.js
@@ -0,0 +1,28 @@
+class Environment {
+  static scriptPath = "https://js.flaw.space/magic/";
+}
+
+const SCRIPTS = ["magic.executor.v2.js", "magic.html.v2.js"];
+
+loadScripts();
+
+function loadScripts() {
+  SCRIPTS.forEach((path, index) => {
+    const script = createScript(path);
+    const lastIndex = SCRIPTS.length - 1;
+
+    if (index == lastIndex) {
+      script.onload = () => {
+        init();
+      };
+    }
+  });
+}
+
+function createScript(path) {
+  const script = document.createElement("script");
+  script.src = Environment.scriptPath + path;
+  script.type = "text/javascript";
+  document.head.append(script);
+  return script;
+}
